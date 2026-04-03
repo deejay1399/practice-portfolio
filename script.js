fb4c26bf48c0a379ab5e372c2cb6a2155100e7eb
diomedes jr hinay <diomedesjrhinay@gmail.com>
2026-04-03 16:35:08 +0800

    changelog: added contact form added animation added styling on the cards

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function sayhello(){
+function sayHello(){
     alert ("Hello! Welcome to my portfolio. Feel free to explore and learn more about me and my work. Thank you for visiting!");
 }
 
 const projects = [
     {
         name: "CCIS Website",
-        description: "A website for the College of Computer and Information Sciences (CCIS) that provides information about courses, faculty, and events."
+        description: "A website for the College of Computer and Information Sciences (CCIS) that provides information about courses, faculty, and events.",
+        image: "assets/images/ccis.png"
     },
     {
         name: "SAS Website",
-        description: "A Website for an NGO designed for underprivileged athletes."
+        description: "A Website for an NGO designed for underprivileged athletes.",
+        image: "assets/images/sas.png"
     },
     {
         name: "WeCare App",
-        description: "A Mobile Application for job seeker and employer matching."
+        description: "A Mobile Application for job seeker and employer matching.",
+        image: "assets/images/wecare.png"
     }
 ];
 
@@ -24,9 +27,29 @@ projects.forEach(p => {
     div.classList.add("project");
 
     div.innerHTML = `
+        <img src="${p.image}" alt="${p.name}">
         <h3>${p.name}</h3>
         <p>${p.description}</p>
         `;
 
     projectList.appendChild(div);
 });
+
+const fadeElements = document.querySelectorAll(".fade-in");
+
+function handleScroll() {
+  fadeElements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 50) {
+      el.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", handleScroll);
+
+function sendMessage(e) {
+  e.preventDefault();
+  alert("Message sent! (we will connect this later)");
+}
